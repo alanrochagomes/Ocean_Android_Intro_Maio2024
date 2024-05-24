1c465a7dc059ebc3547e61d9f7488eadf4dbd4e6
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-24 11:58:38 -0300

    Correção do Desafio

--- app/src/main/java/com/oceanbrasil/ocean_android_intro_maio2024/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_intro_maio2024/MainActivity.kt
@@ -3,6 +3,7 @@ package com.oceanbrasil.ocean_android_intro_maio2024
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +20,12 @@ class MainActivity : AppCompatActivity() {
         // var -> uma variável que pode mudar o valor depois de inicializada
         val btEnviar = findViewById<Button>(R.id.btEnviar)
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome)
 
         btEnviar.setOnClickListener {
             // Dentro desse bloco { }, o código só
             // será executado quando o botão for clicado
-            tvResultado.text = "Paulo Salvatore"
+            tvResultado.text = etNome.text
         }
     }
 }
